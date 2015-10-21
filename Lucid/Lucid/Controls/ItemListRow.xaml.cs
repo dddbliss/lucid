f50ec37f54b1ffca7d1fa94eb0322037664859dc
dddbliss <dddbliss@gmail.com>
2015-10-20 21:45:35 -0700

    Initial commit.
    Initial codebase commit

---
+++ Lucid/Lucid/Controls/ItemListRow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NPLib;
+using System.IO;
+
+namespace Lucid.Controls
+{
+    /// <summary>
+    /// Interaction logic for ItemListRow.xaml
+    /// </summary>
+    public partial class ItemListRow : UserControl
+    {
+        public NPLib.Models.Item Item { get; set; }
+        public BitmapImage ItemBitmap { get; set; }
+
+        public ItemListRow()
+        {
+            InitializeComponent();
+
+            ItemBitmap = new BitmapImage();
+            ItemBitmap.BeginInit();
+
+            var memoryStream = Item.Image.DownloadIntoMemory().Result;
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            ItemBitmap.StreamSource = memoryStream;
+            ItemBitmap.EndInit();
+        }
+    }
+}
